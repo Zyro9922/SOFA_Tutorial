04e6a1fa6aa1e432e6838ae75fcbb6bba3fd3836
Zyro9922 <alihasan9922@gmail.com>
2020-04-23 23:30:34 +0530

    Using iauAf2a

---
+++ cmake-build-debug/CMakeFiles/Launch.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/alihasan/Documents/C++_Programs/Launch/main.cpp" "/Users/alihasan/Documents/C++_Programs/Launch/cmake-build-debug/CMakeFiles/Launch.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/Users/alihasan/Documents/GSOC/astronomy/include"
+  "/Users/alihasan/Documents/GSOC/sofa/20190722/c/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Launch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Launch.dir/main.cpp.o"
+  "Launch"
+  "Launch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Launch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
